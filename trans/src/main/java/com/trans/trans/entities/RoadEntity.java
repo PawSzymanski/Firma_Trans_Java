961be1b1a53bbb403459246652702e3f27a55fef
Dariusz Lelito <lelito.darek@gmail.com>
2020-04-28 19:16:15 +0200

    road entity

---
+++ trans/src/main/java/com/trans/trans/entities/RoadEntity.java
@@ -0,0 +1,28 @@
+package com.trans.trans.entities;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "Kurs")
+@Setter
+@Getter
+public class RoadEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "kurs-sequence")
+    @SequenceGenerator(name = "kurs-sequence", allocationSize = 1)
+    @Column(name = "kur_id", updatable = false, nullable = false)
+    private Long id;
+
+    @Column(name = "kur_data")
+    private LocalDate road;
+
+    @Column(name = "poj_pojazd")
+    private String vehicle;
+
+    @Column(name = "kie_kierowca")
+    private String driver;
+}
