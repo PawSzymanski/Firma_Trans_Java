0e4e2ea863ce88e21b80c57cf4c2798dd26d4e76
pszym <pszyma777@gmail.com>
2020-06-11 13:23:39 +0200

    testy istnienia i działania end-pointów

--- trans/src/test/java/com/trans/trans/TransApplicationTests.java
+++ trans/src/test/java/com/trans/trans/TransApplicationTests.java
@@ -1,13 +1,44 @@
 package com.trans.trans;
 
+import com.trans.trans.jpa.ReservationJpa;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
-class TransApplicationTests {
+@AutoConfigureMockMvc
+public class TransApplicationTests {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private ReservationJpa reservationJpa;
 
     @Test
-    void contextLoads() {
+    public void reservationTest1() throws Exception {
+        mockMvc.perform(get("/api/reservation/all")).andExpect(status().isOk());
+    }
+
+    @Test
+    public void reservationTest2() throws Exception {
+        mockMvc.perform(post("/api/reservation/make")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content("{ \"roadId\": 1, \"clientName\": \"Marek\" }")
+                .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    public void reservationTest3() throws Exception {
+        mockMvc.perform(get("/api/reservation/Marek")).andExpect(status().isOk());
     }
 
 }
